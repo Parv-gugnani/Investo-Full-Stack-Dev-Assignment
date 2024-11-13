94d89de3e48973b38ed802d777f9dd6304ec0acc
Parv <9765.parv@gmail.com>
2024-11-13 15:56:04 +0530

    error solved

--- components/ui/dot-pattern.tsx
+++ components/ui/dot-pattern.tsx
@@ -1,18 +1,18 @@
 import { useId } from "react";
-
 import { cn } from "@/lib/utils";
 
 interface DotPatternProps {
-  width?: any;
-  height?: any;
-  x?: any;
-  y?: any;
-  cx?: any;
-  cy?: any;
-  cr?: any;
+  width?: number;
+  height?: number;
+  x?: number;
+  y?: number;
+  cx?: number;
+  cy?: number;
+  cr?: number;
   className?: string;
-  [key: string]: any;
+  [key: string]: unknown; // Allows other props without using `any`
 }
+
 export function DotPattern({
   width = 16,
   height = 16,
